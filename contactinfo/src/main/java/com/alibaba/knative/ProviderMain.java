4f8cadd1ac4c75d74fa0c6bef106f80a2a5ba8e5
caogu.wyp <caogu.wyp@alibaba-inc.com>
2019-11-15 12:24:26 +0800

    chore:update

--- contactinfo/src/main/java/com/alibaba/knative/ProviderMain.java
+++ contactinfo/src/main/java/com/alibaba/knative/ProviderMain.java
@@ -3,7 +3,6 @@ package com.alibaba.knative;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author caogu.wyp
@@ -12,11 +11,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
-public class ConsumerMain {
+public class ProviderMain {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(ConsumerMain.class);
+        SpringApplication.run(ProviderMain.class);
     }
 }
